7d4e235cb3608509ce506ebce353e38f5ebb24bf
playerowns <pwned901@gmail.com>
2025-04-27 23:43:43 +0300

    Add support for loading OSRS v3 format

--- src/ModelLoader.h
+++ src/ModelLoader.h
@@ -13,6 +13,7 @@ public:
 private:
     static bool LoadAny(ModelData& model, Packet& packet);
     static bool LoadV1(ModelData& model, Packet& packet);
+    static bool LoadOldschoolV3(ModelData& model, Packet& packet);
     static bool LoadMQO(ModelData& model, std::filesystem::path const& filePath);
 
     static bool ConvertFromMQO(ModelData& model, MQOFile const& mqoFile);
